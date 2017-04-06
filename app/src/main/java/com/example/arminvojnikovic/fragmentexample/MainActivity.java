e82d1e995dedd904280812f76ca5c15068e5b9c7
Jon Olsson <aadrianswe@gmail.com>
2017-04-06 20:49:20 +0200

    test commit

--- app/src/main/java/com/example/arminvojnikovic/fragmentexample/MainActivity.java
+++ app/src/main/java/com/example/arminvojnikovic/fragmentexample/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity implements TopFragment.TopSectionListener {
 
+    //myunneccessary comment
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
